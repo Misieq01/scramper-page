18c1cbb74dc5c6d18c5e1d8ed2c4d18433611857
Misieq01 <38589417+Misieq01@users.noreply.github.com>
2021-03-07 16:00:40 +0100

    Updated donwload link

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,7 +8,7 @@ type TButton = {
 const Button = ({ type,style }: TButton) => {
   return type === "download" ? (
     <div className={`button-filled`} style={style}>
-      <a href="https://github.com/Misieq01/Scrapper">Download</a>
+      <a href="https://www.file.io/download/1zCju6BqRCuw">Download</a>
     </div>
   ) : (
     <div className={`button-border`} style={style}>
